a216d36c943ed5e2863e0c623f61b12797ed9887
javier-gallardo-saenz <jgallardosaenz@gmail.com>
2025-01-11 15:48:02 +0100

    Organized in folders

--- pruebas_ErdosRenyi.py
+++ pruebas_ErdosRenyi.py
@@ -9,24 +9,25 @@ from utils import *
 from graph_inference import *
 
 num_nodes = 10000
-prob_edge = 0.05
+prob_edge = 0.005
 r_values = [0, 1]  # radius of the known ball
 methods = {'dmv', 'dwmv', 'dvm', 'dlp'}
+selected_nodes_per_graph = 100
 
-num_iterations = 10
+num_iterations = 5
 avg_aux = {}
 total_results = {method: {r: {'inferred': [], 'true': []} for r in r_values} for method in methods}
 total_weighted_stats = {method: {r: {} for r in r_values} for method in methods}
 
 for n in range(num_iterations):
     G = generate_erdos_renyi_graph(n=num_nodes, p=prob_edge)
-    v = random.sample(list(G.nodes()), 5)  # choose a random set of nodes
+    v = random.sample(list(G.nodes()), selected_nodes_per_graph)  # choose a random set of nodes
     opinion_dist = OpinionDistribution(G)  # create instance of class OpinionDistribution with graph G
-    opinion_dist.initialize_opinions(states=[-1, 0, 1], probabilities=[1/3, 1/3, 1/3], label='opinion')
-    # opinion_dist.basic_opinion_generator(label='opinion', num_steps=10000)
-    opinion_dist.opinion_generator_majority_biased_voter_model(label='opinion', num_iterations=10000, delta=0.1)
+    opinion_dist.initialize_opinions(states=[-1, 0, 1], probabilities=[0.4, 0.2, 0.4], label='opinion')
+    opinion_dist.basic_opinion_generator(label='opinion', num_steps=100000)
+    #opinion_dist.opinion_generator_majority_biased_voter_model(label='opinion', num_iterations=10000, delta=0.1)
     graph_inf = GraphInference(opinion_dist.graph)
-    graph_inf.which_inference_methods()  # shows available inference methods
+    #graph_inf.which_inference_methods()  # shows available inference methods
 
     results_dmv = graph_inf.do_inference(node_set=v, radius_values=r_values, methods=methods, label='opinion',
                                                         count_results=2, clear_results=False, num_iterations=1)
@@ -60,12 +61,14 @@ for method in methods:
 
 
 print(f"Results for Erdos-Renyi with {num_nodes} nodes and {prob_edge} probability of"
-      f" an edge over 1/3 of the node set.")
+      f" an edge over {num_iterations} ER graphs.")
+print(f"Inference was done independently on the frontier of balls centered on "
+      f"{selected_nodes_per_graph} nodes sampled randomly")
 
 for method in avg_aux.keys():
-    print(f"\n{method} :")
+    print(f"\n Method {method} :")
     for r in avg_aux[method].keys():
-        print(f"\t{r} : ")
+        print(f"\t Radius {r} : ")
         for key2 in avg_aux[method][r].keys():
             print(f"\t Iterations averaged {key2} : {avg_aux[method][r][key2]}")
             print(f"\t Inferred nodes averaged {key2} : {total_weighted_stats[method][r][key2]}")
